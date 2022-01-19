f3546f823167d092e1cc2f3985bbc76418c323ff
JoanMBQ <timo_2604@hotmail.com>
2022-01-19 00:47:34 -0500

    Home

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:banca_finanzas/src/widgets/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,30 +6,113 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    bool keyboardIsOpen = MediaQuery.of(context).viewInsets.bottom != 0;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Godares'),
+      backgroundColor: Color(0xff565D82),
+      body: 
+      Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height*0.3,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width*0.07,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                      "GODARES",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w800,
+                        fontFamily: 'Roboto',
+                        letterSpacing: 0.5,
+                        fontSize: 20,
+                        )
+                      ),
+                      SizedBox(height: 25),
+                      CircleAvatar(
+                        minRadius: 50,
+                        backgroundColor: Color(0xffC4C4C4),
+                      ),
+                       SizedBox(height: 25),
+                      Text(
+                      "¡Bienvenido User!",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w800,
+                        fontFamily: 'Roboto',
+                        letterSpacing: 0.5,
+                        fontSize: 20,
+                        )
+                      ),
+                      
+                    ],
+                  ),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        
+                        Icon(
+                            Icons.logout,
+                            color: Colors.white,
+                        ),
+                        Container(
+                          height: MediaQuery.of(context).size.height*0.2,
+                        ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          SingleChildScrollView(
+            child: Container(
+              
+              height: MediaQuery.of(context).size.height*0.605,
+              width: MediaQuery.of(context).size.width*1,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only( 
+                  topLeft: const Radius.circular(50.0),
+                  topRight: const Radius.circular(50.0),
+                  ),
+                color: Color(0xffF0F0F0),
+              ),
+              child: 
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 20),
+                  const Text(
+                    "Listado de empresas encuestadas",
+                    style: TextStyle(
+                      color: Color(0xff565D82),
+                      fontWeight: FontWeight.w800,
+                      fontFamily: 'Roboto',
+                      letterSpacing: 0.5,
+                      fontSize: 20,
+                    )
+                  ),
+                ],
+              ),
+            ),
+          ), 
+        ],
       ),
-      body: Container(
-        padding: EdgeInsets.all(30.0),
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, 'antecedentes');
-              }, 
-              child: Text('Antecedentes Generales')
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, 'antecedentes_empresa');
-              }, 
-              child: Text('Antecedentes de la Empresa')
-            ),
-          ],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Visibility(
+        visible: !keyboardIsOpen,
+        child: FloatingActionButton(
+          backgroundColor: Color(0xff565D82),
+          onPressed: (){},
+          child: Icon(Icons.add),
         ),
       ),
+      bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
 }
